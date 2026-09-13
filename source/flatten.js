955371a2c7d5725bb81557e3253a79257511ffc9
Pom1dorka-Salat1k <andrewzimov97@gmail.com>
2026-09-13 23:39:09 +0300

    refactor: improve code readability

--- source/flatten.js
+++ source/flatten.js
@@ -2,14 +2,13 @@
  * Преобразует вложенный массив в плоский
  *
  * @param {Array} array Вложенный массив
- * @returns {Array} Плоский массив без вложенных массивов
  *
  * @example
  * // returns [1, 2, 3]
  * flatten([1, [2, 3]]);
- * @returns {Array}
+ * 
+ * @returns {Array} Плоский массив без вложенных массивов
  */
-
 const flatten = array => {
     if (!Array.isArray(array)) {
         throw new TypeError("В качестве входного параметра передан неправильный тип, ожидается массив");
